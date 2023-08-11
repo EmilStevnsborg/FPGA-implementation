303263828bee5a5910ad31814b0924ecc7792279
Carl Johnsen <carl-johannes@di.ku.dk>
2023-08-11 09:13:52 +0200

    Added a hack for when multiple processes takes a variable number of input bus arrays

--- CNN/Conv/type00/Filter.cs
+++ CNN/Conv/type00/Filter.cs
@@ -10,15 +10,19 @@ namespace CNN
             get => bias.Output;
             set => bias.Output = value;
         }
-        public Filter(int numInChannels, float[][] weights, float biasVal, 
-                     (int,int) channelSize, (int,int) kernelSize, (int,int) stride, 
+        public Filter(int numInChannels, float[][] weights, float biasVal,
+                     (int,int) channelSize, (int,int) kernelSize, (int,int) stride,
                      (int,int) padding, float padVal
         )
         {
             this.numInChannels = numInChannels;
             kernelOutputs = new ValueBus[numInChannels];
             convKernels = new ConvKernel_type00[numInChannels];
-            valueArrayCtrl = new ValueArrayCtrl(numInChannels, channelSize);
+            // TODO Hack for when the array of buses are only length 1.
+            if (numInChannels > 1)
+                valueArrayCtrl = new ValueArrayCtrl(numInChannels, channelSize);
+            else
+                valueArrayCtrl_single = new ValueArrayCtrl_single(numInChannels, channelSize);
             plusCtrl = new PlusCtrl();
             bias = new Bias(biasVal);
             for (int i = 0; i < numInChannels; i++)
@@ -29,14 +33,25 @@ namespace CNN
                 kernelOutputs[i] = convKernel.Output;
             }
             // connect busses
-            valueArrayCtrl.Input = kernelOutputs;
-            plusCtrl.Input = valueArrayCtrl.Output;
+            // TODO Hack for when the array of buses are only length 1.
+            if (numInChannels > 1)
+            {
+                valueArrayCtrl.Input = kernelOutputs;
+                plusCtrl.Input = valueArrayCtrl.Output;
+            }
+            else
+            {
+                valueArrayCtrl_single.Input = kernelOutputs[0];
+                plusCtrl.Input = valueArrayCtrl_single.Output;
+            }
             bias.Input = plusCtrl.Output;
         }
         private int numInChannels;
         public ConvKernel_type00[] convKernels;
         private ValueBus[] kernelOutputs;
         private ValueArrayCtrl valueArrayCtrl;
+        // TODO Hack for when the array of buses are only length 1.
+        private ValueArrayCtrl_single valueArrayCtrl_single;
         private PlusCtrl plusCtrl;
         private Bias bias ;
     }
--- CNN/General/ValueArrayCtrl.cs
+++ CNN/General/ValueArrayCtrl.cs
@@ -61,4 +61,55 @@ namespace CNN
 
         }
     }
+
+    [ClockedProcess]
+    public class ValueArrayCtrl_single : SimpleProcess
+    {
+        [InputBus]
+        public ValueBus Input;
+        [OutputBus]
+        public ValueBus Output = Scope.CreateBus<ValueBus>();
+
+        private SME.VHDL.UInt3 numInChannels; // (5 dec)
+        private SME.VHDL.UInt5 channelHeight; // (26 dec)
+        private SME.VHDL.UInt5 channelWidth;  // (26 dec)
+        private SME.VHDL.UInt3 i = 0;         // (5 dec)
+        private SME.VHDL.UInt3 k = 0;         // (5 dec)
+
+        private float buffer;
+        public ValueArrayCtrl_single(int numInChannels, (int, int) channelSize)
+        {
+            this.numInChannels = (SME.VHDL.UInt3) numInChannels;
+            this.channelHeight = (SME.VHDL.UInt5) channelSize.Item1;
+            this.channelWidth = (SME.VHDL.UInt5) channelSize.Item2;
+        }
+        protected override void OnTick()
+        {
+            Output.enable = Output.LastValue = false;
+            // Output should only be updated when the input is valid.
+            if (Input.enable)
+            {
+                buffer = Input.Value;
+                i++;
+            }
+
+            // If Inputs have loaded go through them
+            if (i > 0 && k < i)
+            {
+                Output.Value = buffer;
+                Output.enable = true;
+                k++;
+                if (k % numInChannels == 0)
+                {
+                    Output.LastValue = true;
+                }
+                if (k == i)
+                {
+                    i = 0;
+                    k = 0;
+                }
+            }
+        }
+    }
+
 }
