42a9d32712c67f39a954c529c2b3ebd3d9f20c08
Carl Johnsen <carl-johannes@di.ku.dk>
2023-08-11 07:02:24 +0200

    Added ReluCore implementation

--- CNN/Relu/ReluCore.cs
+++ CNN/Relu/ReluCore.cs
@@ -1,5 +1,6 @@
 using System;
 using SME;
+using SME.VHDL;
 
 namespace CNN
 {
@@ -12,6 +13,49 @@ namespace CNN
 
         [OutputBus]
         public ValueBus Output = Scope.CreateBus<ValueBus>();
+
+        public class Renderer : ICustomRenderer
+        {
+            public string IncludeRegion(RenderStateProcess rsp, int indentation)
+            {
+                return "";
+                //return "use IEEE.float_pkg.all;";
+            }
+
+            public string BodyRegion(RenderStateProcess rsp, int indentation)
+            {
+                return @"
+begin
+    process(
+        CLK,
+        RST
+    )
+    begin
+        if (RST = '1') then
+            Output_enable <= '0';
+            Output_LastValue <= '0';
+            Output_Value <= (others => '0');
+            FIN <= '0';
+        elsif rising_edge(CLK) then
+            Output_enable <= Input_enable;
+            Output_LastValue <= '0';
+            if (Input_enable = '1') then
+                if Input_Value(31) = '0' then
+                    Output_Value <= Input_Value;
+                else
+                    Output_Value <= (others => '0');
+                end if;
+            else
+                Output_Value <= (others => '0');
+            end if;
+            FIN <= not RDY;
+        end if;
+    end process;
+";
+            }
+        }
+        [Ignore] private Renderer renderer = new Renderer();
+        public override object CustomRenderer { get => renderer; }
 
         protected override void OnTick()
         {
