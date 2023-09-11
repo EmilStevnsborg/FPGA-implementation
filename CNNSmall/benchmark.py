72780ce577b84dc2b563679b75bea39e510bfcbe
Carl Johnsen <cjjohnsen@nbi.ku.dk>
2023-09-11 13:10:09 +0200

    Pretty printing of benchmarks

--- CNNSmall/benchmark.py
+++ CNNSmall/benchmark.py
@@ -5,13 +5,13 @@ import numpy as np
 import torch
 
 # Benchmarking parameters
-cpu = False
+cpu = True
 dev = 'cpu' if cpu else 'cuda:0'
 device = torch.device(dev)
-n_samples = 4000
-batch_size = 2000
-warmup = 5
-runs = 10
+n_samples = 1000
+batch_size = 10
+warmup = 10
+runs = 1000
 
 # Load the model
 model = arch.CNNSmall()
@@ -50,13 +50,42 @@ with torch.no_grad():
                 print(preds[j*batch_size:(j+1)*batch_size])
                 print(np.abs(res.cpu().numpy() - preds[j*batch_size:(j+1)*batch_size]))
                 exit(1)
-    start = datetime.datetime.now()
+
+    to_device = []
+    compute = []
+    from_device = []
+    
     for i in range(runs):
         for j in range(n_samples // batch_size):
-            input_tensor = torch.Tensor(test_data[j*batch_size:(j+1)*batch_size]).to(device)
-            model(input_tensor).cpu() # Include data transfer time
-    end = datetime.datetime.now()
+            input_tensor = torch.Tensor(test_data[j*batch_size:(j+1)*batch_size])
+            start = datetime.datetime.now()
+            input_tensor = input_tensor.to(device)
+            transferred = datetime.datetime.now()
+            res = model(input_tensor)
+            computed = datetime.datetime.now()
+            res = res.cpu() # Include data transfer time
+            end = datetime.datetime.now()
 
-    print(f'Time for {runs} runs of {n_samples} samples: {end - start}')
-    print(f'Average time per run: {(end - start) / runs}')
-    print(f'Average time per sample: {(end - start) / (runs * (n_samples//batch_size)*batch_size)}')
+            to_device.append((transferred - start).total_seconds() * 1e6)
+            compute.append((computed - transferred).total_seconds() * 1e6)
+            from_device.append((end - computed).total_seconds() * 1e6)
+    
+    to_device = np.array(to_device)
+    compute = np.array(compute)
+    from_device = np.array(from_device)
+
+    mean_to_device = np.mean(to_device)
+    std_to_device = np.std(to_device)
+    mean_compute = np.mean(compute)
+    std_compute = np.std(compute)
+    mean_from_device = np.mean(from_device)
+    std_from_device = np.std(from_device)
+    total = (to_device + compute + from_device).sum()
+    mean_total = np.mean(to_device + compute + from_device)
+    std_total = np.std(to_device + compute + from_device)
+
+    print(f'To device:           {mean_to_device:11.02f} us (+/- {std_to_device:11.02f})')
+    print(f'Compute:             {mean_compute:11.02f} us (+/- {std_compute:11.02f})')
+    print(f'From device:         {mean_from_device:11.02f} us (+/- {std_from_device:11.02f})')
+    print(f'Combined:            {mean_total:11.02f} us (+/- {std_total:11.02f})')
+    print(f'Combined per sample: {(mean_total / batch_size):11.02f} us (+/- {(std_total / batch_size):11.02f})')
